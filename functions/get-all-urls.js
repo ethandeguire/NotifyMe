ce789190fe1b53bc36f0ffcde4a329a7f5147298
Ethan <ethandeguire@gmail.com>
2019-08-06 21:37:30 -0400

    update-url-in-db

--- functions/get-all-urls.js
+++ functions/get-all-urls.js
@@ -13,6 +13,8 @@
 
 import faunadb from 'faunadb' // Import faunaDB sdk
 
+const _COLLECTION_NAME = "url"
+
 // configure faunaDB Client with our secret
 const q = faunadb.query
 const client = new faunadb.Client({
@@ -22,28 +24,29 @@ const client = new faunadb.Client({
 // export our lambda function as named "handler" export
 exports.handler = (event, context, callback) => {
   // tell the console:
-  console.log('Function `get-all-urls` invoked')
+  console.log(`--Function 'get-all-${_COLLECTION_NAME}' invoked`)
 
-  return client.query(q.Paginate(q.Match(q.Ref('indexes/all_urls'))))
+  return client.query(q.Paginate(q.Match(q.Ref(`indexes/all_${_COLLECTION_NAME}`))))
     .then((response) => {
-      const urlRefs = response.data
-      console.log('urls:', urlRefs)
       console.log(`${urlRefs.length} urls found`)
+      const refs = response.data
+      console.log(`--${_COLLECTION_NAME}:`, refs)
+      console.log(`--${refs.length} ${_COLLECTION_NAME} found`)
 
-      // create new query out of urls refs
-      const getAllUrlsDataQuery = urlRefs.map((ref) => {
+      // create new query out of refs
+      const getAllDataQuery = refs.map((ref) => {
         return q.Get(ref)
       })
 
       // then query the refs
-      return client.query(getAllUrlsDataQuery).then((ret) => {
+      return client.query(getAllDataQuery).then((ret) => {
         return callback(null, {
           statusCode: 200,
           body: JSON.stringify(ret)
         })
       })
     }).catch((error) => {
-      console.log('error', error)
+      console.log('--error', error)
       return callback(null, {
         statusCode: 400,
         body: JSON.stringify(error)
---
+++ functions/update-url-in-db.js
@@ -0,0 +1,33 @@
+// ------ Definitions ------
+//
+// JSON Input Data:
+//  {
+//    [
+//      "{USERNAME}",
+//      "{PASSWORD}"
+//    ]
+//  }
+//
+// ------ /Definitions -----
+
+import faunadb from 'faunadb' // Import faunaDB sdk
+
+// configure faunaDB Client with our secret
+const q = faunadb.query
+const client = new faunadb.Client({
+  secret: process.env.FAUNADB_SERVER_SECRET
+})
+
+// export our lambda function as named "handler" export
+exports.handler = (event, context, callback) => {
+  // parse the string body into a useable JS object 
+  const body = JSON.parse(event.body)
+
+  // tell the console:
+  console.log('Function `add-url-to-db` invoked')
+
+  // create a document in collection urls with data from the event
+  return client.query(q.Paginate(q.Match(q.Index("all_urls"), ["newtest", "newtest"])))
+  .then((result) => {console.log(result)})
+  .catch((error) => {console.log(error)})
+}
