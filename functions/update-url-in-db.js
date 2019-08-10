e67eb15a9ec5ab686447465a45af7f95d5afc3b8
Ethan <ethandeguire@gmail.com>
2019-08-09 21:06:00 -0400

    Update update-url-in-db.js

--- functions/update-url-in-db.js
+++ functions/update-url-in-db.js
@@ -13,6 +13,8 @@
 
 import faunadb from 'faunadb' // Import faunaDB sdk
 
+const _COLLECTION_NAME = "urls"
+
 // configure faunaDB Client with our secret
 const q = faunadb.query
 const client = new faunadb.Client({
@@ -21,31 +23,51 @@ const client = new faunadb.Client({
 
 // export our lambda function as named "handler" export
 exports.handler = (event, context, callback) => {
-  // parse the string body into a useable JS object 
-  const body = JSON.parse(event.body)
+
+  let body = JSON.parse(event)
 
   // tell the console:
-  console.log('Function `add-url-to-db` invoked')
+  console.log(`--Function 'get-all-${_COLLECTION_NAME}' invoked`)
 
-  // create a document in collection urls with data from the event
-  return client.query(
-    q.Update(
-      q.Select(
-        q.Get(
-          q.Match(
-            q.Index("urls_by_username"), 
-            "uniqueusername"
-          )
-        )
-      ), 
-      body
-    )
-  )
-  .then((result) => {console.log("--SUCCESS:" + JSON.stringify(result))})
-  .catch((error) => {console.log("--ERROR:" + JSON.stringify(error))})
+  return client.query(q.Paginate(q.Match(q.Ref(`indexes/all_${_COLLECTION_NAME}`))))
+    .then((response) => {
+      const refs = response.data
+      console.log(`--${_COLLECTION_NAME}:`, refs)
+      console.log(`--${refs.length} ${_COLLECTION_NAME} found`)
 
+      // create new query out of refs
+      const getAllDataQuery = refs.map((ref) => {
+        return q.Get(ref)
+      })
 
-  // return client.query(q.Paginate(q.Match(q.Index("all_urls"), "newtest")))
-  // .then((result) => {console.log("--SUCCESS:" + JSON.stringify(result))})
-  // .catch((error) => {console.log("--ERROR:" + JSON.stringify(error))})
+      // then query the refs
+      return client.query(getAllDataQuery).then((jsonArray) => {
+        
+        let objects = JSON.parse(jsonArray)
+        console.log("--Got all objects, looping through to find matching")
+
+        // find the 
+        objects.forEach(obj => {
+          let objBody = obj.data
+          console.log(body.data.username, objBody.data.username, body.data.password, objBody.data.password)
+          if (body.data.username == objBody.data.username && body.data.password == objBody.data.password){
+            return client.query(objBody["@ref"]["id"])
+              .then ((returnVal) => console.log("--Update return statement: " + returnVal))
+          }
+        });
+
+        // return callback(null, {
+        //   statusCode: 200,
+        //   body: JSON.stringify(ret)
+        // })
+
+      })
+    }).catch((error) => {
+      console.log('--error', error)
+      return callback(null, {
+        statusCode: 400,
+        body: JSON.stringify(error)
+      })
+    })
+}
 }
