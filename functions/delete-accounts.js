0493f4a89cfa51f30d563cc02179f8d8571fdd71
Ethan <ethandeguire@gmail.com>
2019-08-11 13:49:05 -0400

    changes

--- functions/delete-accounts.js
+++ functions/delete-accounts.js
@@ -41,9 +41,15 @@ exports.handler = (event, context, callback) => {
         .then((objects) => {
           for (let i = 0; i < refs.length; i++) {
             let obj = objects[i]
+        
+            // figure out if we should delete this one
+            let deleteThisOne = false
+            accountsToDelete.forEach(account => {
+              if (account.username == obj.data.username && account.password == obj.data.password) deleteThisOne = true
+            });
             
-            // delete if
-            if (account.username == obj.data.username && account.password == obj.data.password){
+            // delete if we should delete this one
+            if (deleteThisOne){
               return client.query(q.Delete(refs[i]))
                 .then((returnVal) => {
                   console.log(`--Delete user '${obj.data.username}' successful`)
