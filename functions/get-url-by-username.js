ea15b37570631d8f25c6df0b83f433042ef2852e
Ethan <ethandeguire@gmail.com>
2019-08-11 21:14:22 -0400

    Create get-url-by-username.js

---
+++ functions/get-url-by-username.js
@@ -0,0 +1,60 @@
+// ------ Definitions ------
+//
+// JSON Return Data:
+//  {
+//    data: {
+//      username: "{username}",
+//      password: "{password}",
+//      url: "url",
+//    }
+//  }
+//
+// ------ /Definitions -----
+
+import faunadb from 'faunadb' // Import faunaDB sdk
+
+const _COLLECTION_NAME = "urls"
+
+// configure faunaDB Client with our secret
+const q = faunadb.query
+const client = new faunadb.Client({
+  secret: process.env.FAUNADB_SERVER_SECRET
+})
+
+// export our lambda function as named "handler" export
+exports.handler = (event, context, callback) => {
+  // tell the console:
+  console.log(`--Function 'get-all-${_COLLECTION_NAME}' invoked`)
+
+  let username = JSON.parse(event.body).username
+
+  return client.query(q.Paginate(q.Match(q.Ref(`indexes/all_${_COLLECTION_NAME}`))))
+    .then((response) => {
+      const refs = response.data
+      console.log(`--${_COLLECTION_NAME}:`, refs)
+      console.log(`--${refs.length} ${_COLLECTION_NAME} found`)
+
+      // create new query out of refs
+      const getAllDataQuery = refs.map((ref) => {
+        return q.Get(ref)
+      })
+
+      // then query the refs
+      return client.query(getAllDataQuery).then((urlObjects) => {
+        urlObjects.forEach(urlObject => {
+          if (username == urlObject.data.username){
+            return callback(null, {
+              statusCode: 200,
+              body: JSON.stringify(urlObject.data.url)
+            })
+          }
+        });
+      })
+    }).catch((error) => {
+      console.log('--error', error)
+      return callback(null, {
+        statusCode: 400,
+        body: JSON.stringify(error)
+      })
+    })
+}
