1849d3700a18979f8d120aaeb0cda9147a265d4f
Ethan <ethandeguire@gmail.com>
2019-08-11 20:56:54 -0400

    Update notif.js

--- functions/notif.js
+++ functions/notif.js
@@ -9,12 +9,21 @@ exports.handler = (event, context, callback) => {
   // tell the console:
   console.log(`--Function 'notif' invoked`)
 
-  let body = JSON.parse(event.body)
-  let params = JSON.parse(event.queryStringParameters)
+  try {
+    console.log(typeof event.queryStringParameters, event.queryStringParameters)
+    console.log(typeof event.body, event.body)
+  } catch (err) { console.log("1:", err) }
 
-  console.log("--params:", params)
-  console.log("body:", body)
-  
+  try {
+    let params = JSON.parse(event.queryStringParameters)
+    let body = JSON.parse(event.body)
+  } catch (err) { console.log("2:", err) }
+
+  try {
+    console.log("--params:", params)
+    console.log("body:", body)
+  } catch (err) { console.log("3:", err) }
+
   return callback(null, {
     statusCode: 200,
     message: "success"
