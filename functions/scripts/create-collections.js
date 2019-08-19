dcb6f887082b4fd7787321f3a7072c7a19f18110
Ethan <ethandeguire@gmail.com>
2019-08-18 20:20:24 -0400

    fixed createcollections

--- functions/scripts/create-collections.js
+++ functions/scripts/create-collections.js
@@ -1,15 +1,26 @@
-import faunadb from 'faunadb' // Import faunaDB sdk
+const faunadb = require('faunadb') // Import faunaDB sdk
 
 // configure faunaDB Client with our secret
 const q = faunadb.query
 const client = new faunadb.Client({
-  secret: process.env.FAUNADB_SERVER_SECRET
+  secret: 'fnADVEKXmjACASmqtZzNdLcl3JpIVGzc7Yxzfsk9'
 })
 
 const collections = ["urls", "authentications"]
+let errors = []
 
 collections.forEach(collection => {
   client.query(q.CreateCollection({ name: collection }))
-    .then((ret) => console.log(ret))
-    .catch((err) => console.log(err))
+    .catch((err) => { if (err.message !== 'instance already exists') errors.push({ name: error.name, message: error.message }) })
+
+  client.query(q.CreateIndex({ name: `all_${collection}`, source: q.Collection(collection) }))
+    .catch((err) => { if (err.message !== 'instance already exists') errors.push({ name: error.name, message: error.message }) })
+
 });
+
+if (errors.length != 0) {
+  console.log(`Error(s) in create-collection.js: ${errors}`)
+  throw new Error(`Error(s) in create-collection.js: ${errors}`)
+}
+
+console.log(`collections exist:'${[collections]}'`)
