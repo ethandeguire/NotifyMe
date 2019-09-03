7ec7656ca8802e906367880012f5f327c394d848
Ethan <ethandeguire@gmail.com>
2019-09-02 21:09:36 -0400

    Update validate-session-token.js
    should return 200 on invalid session tokens

--- functions/validate-session-token.js
+++ functions/validate-session-token.js
@@ -1,15 +1,18 @@
-
-const AUTHENTICATION_EXPIRATION_TIME_MINS = 120
-
 // import my functions
 const helpers = require('./tools/helpers')
 const callbackPackager = helpers.callbackPackager
 const getObjectByUsernameAndCollection = helpers.getObjectByUsernameAndCollection
 
+// local variables, could be an env var or something idk
+const AUTHENTICATION_EXPIRATION_TIME_MINS = 120
+
 // export our lambda function as named "handler" export
 exports.handler = (event, context, callback) => {
 
-  const data = JSON.parse(event.body).data
+  // check if this is a preflight request, if it is, return 200
+  if (event.httpMethod == 'OPTIONS') return callbackPackager(callback, 200, { success: "OPTIONS request" })
+
+  const data = JSON.parse(event.body)['data']
   const [username, session_token] = [data["username"], data["session_token"]]
 
   // tell the console:
@@ -25,11 +28,12 @@ exports.handler = (event, context, callback) => {
 
       // if the token is correct and not older than the expiry period, return valid
       if (urlObject['session_token'] == session_token && difMins < AUTHENTICATION_EXPIRATION_TIME_MINS) {
-        return callbackPackager(callback, 200, { data: { is_valid: true , mins_left: AUTHENTICATION_EXPIRATION_TIME_MINS - difMins} })
-      } 
+        return callbackPackager(callback, 200, { data: { is_valid: true, mins_left: AUTHENTICATION_EXPIRATION_TIME_MINS - difMins } })
+      }
 
       // if its not valid, return that
-      else return callbackPackager(callback, 400, { data: { is_valid: false } })
-      
+      else return callbackPackager(callback, 200, { data: { is_valid: false } })
+
     })
+    .catch(err => callbackPackager(callback, 500, { error: err }))
 }
